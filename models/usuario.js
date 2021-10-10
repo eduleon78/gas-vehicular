14b32029eb8e83d219f3625d4b653f9a58aee5c6
eduleon78 <66341077+eduleon78@users.noreply.github.com>
2021-10-09 23:02:35 -0400

    se extrajo el gitinore

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,52 @@
 var mongoose = require('mongoose');
 var Reserva = require('./reserva');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
 var Schema = mongoose.Schema;
 
+const validateEmail = function(email){
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
 var usuarioSchema = new Schema({
-    nombre: String,
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es obligarorio']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        validate: [validateEmail, 'Por favor ingrese un email valido'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
 });
+
+usuarioSchema.pre('save', function(next){
+    if(this.isModified('passwprd')){
+        this.password = bcrypt.hashSync(this.password, saltRound);
+    }
+    next();
+});
+
+usuarioSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
 
 usuarioSchema.methods.reservar = function(vehiId, desde, hasta, cb){
     var reserva = new Reserva({usuario: this._id, vehiculo: vehiId, desde: desde, hasta: hasta, cb: cb});
